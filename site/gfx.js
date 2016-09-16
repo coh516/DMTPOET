089aea8a231b87183d4653d4fb2dd801a3f39994
Seth T <coh516@gmail.com>
2016-09-16 18:00:23 -0400

    fix js error

--- site/gfx.js
+++ site/gfx.js
@@ -7,7 +7,7 @@ Copywrite 2013, 4thTemple.com, FourthTemple.com
 
 
 // this needs to be done a bit better...
-// moveTo is sketchy ..... 
+// moveTo is sketchy .....
 zIndex = {};
 zIndex["ptr"] = 100;
 zIndex["svg"] = 1000;
@@ -21,7 +21,7 @@ zIndex["renderedApps"] = 12000
 
 
 // this needs to be merged with the general graph
-// the best way to manage node references is to classify them by ID only and use getElementByID... 
+// the best way to manage node references is to classify them by ID only and use getElementByID...
 // obvious source of memory leaks..
 // need to update it...
 
@@ -50,21 +50,21 @@ Gfx.prototype = {
 		if (!renderer)
 			this.renderer = htmlRenderer.prototype;
 
-	
-	
+
+
 		this.renderer  = renderer.prototype;
 
 		var ng = 0;
 
 		this.id = mkguid();
-		//the link curve still needs to be refactored to be 
+		//the link curve still needs to be refactored to be
 		//managed by seperate id's...
 
 
 		gfxLookup[this.id] = this;
 
 		var o = getObject(ptr, graphLookup);
-	
+
 		if (!o.gfx) {
 			o.gfx = {};
 		}
@@ -74,12 +74,12 @@ Gfx.prototype = {
 
 
 		// baseElement is when there is an object within another object
-		// the external object's responsibility to set it 
+		// the external object's responsibility to set it
 		var be = baseElement ? true : false;
 		if (!baseElement)
 			baseElement = document.body;
 		this.baseElement = baseElement;
-		
+
 		o.gfx[this.type]['baseElement'] = baseElement;
 		o.gfx[this.type]['type'] = type;
 	        o.gfx[this.type]['rootPtr'] = ptr;
@@ -89,20 +89,22 @@ Gfx.prototype = {
 		// not sure if setting the type is necessary
 
 		this.gfxPtr = ptr.concat(['gfx', this.type]);
-		if (!Gfx.prototype.setted) 
+		if (!Gfx.prototype.setted)
 			this.setupNodeEvents();
 		Gfx.prototype.setted = true;
 
 	},
+
+	"hasIndex":function() { this.rootGfxObj._index = false; },
 	get graphGfxPtr() { return this.rootPtr.concat(['gfx', this.type]) },
 	get rootGfxObj() { return getObject(this.graphGfxPtr, graphLookup) },
-	set hasIndex() { this.rootGfxObj._index = false; },
+
 	get hasIndex() { if (this.rootGfxObj.hasOwnProperty("_index")) {
 				return this.rootGfxObj._index;
 			}
-			else return true; 
+			else return true;
 	},
-	"getGfx":function(ptr) { 
+	"getGfx":function(ptr) {
 		var o = getGraphObject(ptr);
 		return o['gfx'][this.type];
 	},
@@ -110,7 +112,7 @@ Gfx.prototype = {
 		var gfx = this.getGfx(ptr);
 		if (bool) {
 			gfx.hideItem = true;
-			this.hideChildren(ptr);			
+			this.hideChildren(ptr);
 		}
 	},
 	"hideItem":function(ptr) {
@@ -127,9 +129,9 @@ Gfx.prototype = {
 		var type = this.type;
 		graphLookup[this.graphId].recurseItems(
 				function(ptr) {
-					var o = getGraphObject(ptr); 
+					var o = getGraphObject(ptr);
 					if (!o.gfx)o.gfx = {};
-				       	o.gfx[type] = {} 
+				       	o.gfx[type] = {}
 				}
 				)
 
@@ -139,22 +141,22 @@ Gfx.prototype = {
 	// type = "inputBox" -- then re-indexed
 	// after refresh, should it display (it should go through a subtle intermediate phase)
 	//
-	// as well, these specific node things should be subsetted with their respective functions ... 
+	// as well, these specific node things should be subsetted with their respective functions ...
 
-		
+
 
 	"setupNodeEvents": function () {
 		var cfe = Gfx.prototype.checkForEnter;
 		Gfx.prototype.c4n = cfe;
 		window.addEventListener('keypress', cfe, true);
-	
+
 	},
 	"checkForEnter": function(e) {
-	      	
+
 		if (e.keyCode == 13 && Gfx.prototype.isRenaming) {
-				
+
 			var n = Gfx.prototype.deselectInputBox.bind({id:this.id})();
-			
+
 		}
 	},
 	//maybe extend this for UI gfx
@@ -162,16 +164,16 @@ Gfx.prototype = {
 		var v = getGraphObject(ptr.concat(['gfx', this.type])).div;
 		return v.selectedIndex;e
 
-	},	
+	},
 	"getValue":function(ptr) {
 		var v = getGraphObject(ptr.concat(['gfx', this.type])).div.value;
 		return v ? v : getGraphObject(ptr).value;
-		
+
 	},
 	"setInputBox":function(ptr) {
-	
+
 		getGraphObject(ptr.concat(['gfx', this.type])).objType = 'inputBox';
-	       this.rebuild();	
+	       this.rebuild();
 	},
 	"setLabel":function(ptr) {
 		getGraphObject(ptr.concat['gfx', this.type]).objType = 'label';
@@ -187,19 +189,19 @@ Gfx.prototype = {
 		var rect = Gfx.prototype.isRenaming.rect;
 		var ptr = rect.ptr;
 		if (ptr) {
-			
+
 			var o = getObject(ptr, graphLookup);
 			//once again.. need to store id's...
 			if (!o.inputBox) console.log(o);
 			var it = o.inputBox.value;
-			
+
 			o.inputBox.parentNode.removeChild(o.inputBox);
-			o.hiddenInputBox.parentNode.removeChild(o.hiddenInputBox);	
-			
+			o.hiddenInputBox.parentNode.removeChild(o.hiddenInputBox);
+
 			var p = copyArray(ptr);
-			p.pop(); 
+			p.pop();
 			var id = ptr[0];
-		
+
 			graphObjLookup[id].renamePtr(rect.nodeRoot, it);
 			Gfx.prototype.isRenaming = false;
 			delete events[id];
@@ -222,10 +224,10 @@ Gfx.prototype = {
 
 		ib.type = "text";
 		ibc.type = "text";
-		ib.value = item.value; 
+		ib.value = item.value;
 		d.appendChild(ib);
-	
-	
+
+
 		var xy = getElPos(d);
 		ib.style.visibility = "hidden";
 		ibc.value = item.value;
@@ -237,7 +239,7 @@ Gfx.prototype = {
 			o.inputBox = ibc;
 		o.hiddenInputBox = ib;
 		this.reindex();
-	
+
 		Gfx.prototype.isRenaming = {"rect":rect, "gfxId":this.id};
 
 	},
@@ -247,7 +249,7 @@ Gfx.prototype = {
 		var o = getObject(ptr, graphLookup);
 		graphObjLookup[ptr[0]].switchType(ptr, type);
 		this.rebuild();
-		
+
 
 	},
 
@@ -261,7 +263,7 @@ Gfx.prototype = {
 		o.layout = "list";
 		this.rebuild();
 	},
-	
+
 	"build":function(cb) {
 		this.renderer.setElement(this.rootGfxObj);
 		this.renderer.mkPtrImgs(this.rootGfxObj);
@@ -279,17 +281,17 @@ Gfx.prototype = {
 
 		snapSpace.prototype.updateSnapObject(this.graphId, "graphLookup");
 		this.renderer.reindex(this.rootGfxObj);
-	
+
 	},
 
 
 
 	"doSort":function(a,b) {
-	
+
 		return a.props.index-b.props.index;
 	},
 	"sortIM":function(g) {
-	
+
 		if (Array.isArray(g))
 			g.sort(Gfx.prototype.doSort); // gfx contains dom references, can't use closures.
 		else return;
@@ -302,23 +304,23 @@ Gfx.prototype = {
 	},
 
 	"hide":function() {
-	
+
 		var o = getObject(this.gfxPtr, graphLookup);
-	
+
 		this.renderer.hide(o);
 	},
 	"isHidden":function() {
 		var o = getObject(this.gfxPtr, graphLookup);
-		return o.hidden;	
+		return o.hidden;
 	},
 	"show":function() {
 		var o = getObject(this.gfxPtr, graphLookup);
 		this.renderer.show(o);
 	},
-	"showChildren":function(ptr) { 
+	"showChildren":function(ptr) {
 		var o = getObject(ptr, graphLookup);
 		o.hideChildren = false;
-	
+
 		this.rebuild();
 	},
 	"hideChildren":function(ptr) {
@@ -329,7 +331,7 @@ Gfx.prototype = {
 		this.rebuild();
 	},
 	"hideItem":function(ptr) {
-	
+
 		var o = getObject(ptr, graphLookup);
 		o.hideItem = true;
 
@@ -348,43 +350,43 @@ Gfx.prototype = {
 			var n = n[i]["item"];
 		n[endPoint]["item"] = n[old-1]["item"]
 	},
-	
+
 	"ptrDelete":function(ptr) {
 		delete ptrs[ptr];
 		// check for id content length to see if object gets totally deleted
 	},
-	// to redraw lines after something has been deleted, delete everything from 
+	// to redraw lines after something has been deleted, delete everything from
 	// parent node's id off the curveLookup... and just redraw everything
-	
+
 	"deleteCurve":function(indexedPtr) {
 
-	// reindex code...	
+	// reindex code...
 		var ids = [];
 		if (indexedPtr[indexedPtr.length-2] == "index") {
 			var idn = indexedPtr.pop();
 			indexedPtr.pop();
-		}else idn = 0; 
+		}else idn = 0;
 		var gil = graphLookup[this.id].getItemList(indexedPtr);
 		for (var i =0; i < gil.length; i++) {
 			var index = getObject(gil[i], graphLookup).index;
 			if (index) {
 				var limit = idn ? idn+1 : index.length;
-					
+
 				for (var idx = idn; idx < index.length; idx++) {
 					var parents = index[idx].parents;
 					var children = index[idx].children;
 					for (var j = 0; j < parents.length; j++) {
 
 						var cp = copyArray(parents[j]);
-						
+
 						var cpi = cp.pop();
 						var o = getObject(parents[cp], graphObject);
 						o.splice(cpi, 1);
-						
+
 						curveLookup[parents[j]][gil[i].concat(["index", idx])].delete();
 						index[idx].parents.splice(j, 1);
 
-					
+
 					}
 
 					for (var j = 0; j < children.length; j++) {
@@ -396,7 +398,7 @@ Gfx.prototype = {
 
 						curveLookup[gil[i].concat(["index", idx])][children[j]].delete();
 						index[idx].children.splice(j, 1);
-					
+
 						// need to delete now the parent element references
 					}
 				}
@@ -407,11 +409,11 @@ Gfx.prototype = {
 
 	},
 
-		
+
 	"connect":function(c1, c2) {
-		console.log(c1+ " "+c2);	 
+		console.log(c1+ " "+c2);
 		pt = Graph.prototype.connect(c1.indexRoot, c2.indexRoot);
-	
+
 		if (!pt) return;
 		gfxLookup[c1.gfxId].rebuild();
 
@@ -426,7 +428,7 @@ Gfx.prototype = {
 	"moveCanvas":function() {
 		this.renderer.moveGfx(this.rootGfxObj);
 		if (typeof this._moveCanvas === 'function') this._moveCanvas();//this.drawCurves();
-		
+
 		return;
 		var t = this.model.loc;
 		var canvas = gfxLookup[this.id].canvas;
@@ -441,8 +443,8 @@ Gfx.prototype = {
 		tml.loc.x = x;
 
 		this.moveCanvas();
-			
-	
+
+
 	},
 	"setY":function(y) {
 		var tml = this.rootGfxObj
@@ -459,7 +461,7 @@ Gfx.prototype = {
 			tml.y = y;// = { "x":x, "y":y};
 			this.moveCanvas();
 		}
-	
+
 	},
        	"setZ":function(z) {
 		this.rootGfxObj.loc.z = z;
@@ -467,7 +469,7 @@ Gfx.prototype = {
 
 
 	"mkButton":function(ptr) {
-	},	
+	},
 	"setXYZ":function(x,y,z) {
 		var tml = this.rootGfxObj.loc
 		tml.x = x;
@@ -478,13 +480,11 @@ Gfx.prototype = {
 		this.moveCanvas();
 
 	},
-	// most likely this will be refactored out into something more generic ... 
+	// most likely this will be refactored out into something more generic ...
 
 	"ptr2InputBox":function(ptr) {
 	}
 
 
-	
+
 }
-
-
